c868dcc91cbf1852d2610cadea026a57228fd966
Sarah-Lee Bester <sarahleebester@gmail.com>
2024-04-07 14:27:20 +0000

    feat(home): Add home app

--- natures_whisper/urls.py
+++ natures_whisper/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from login.views import login_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', login_view, name='login_view')
+    path('login/', login_view, name='login_view'),
+    path("", include("home.urls"), name="home-urls"),
 ]
