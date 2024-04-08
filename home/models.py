a7973d96784b3ab43434117327405759d376f76d
Sarah-Lee Bester <sarahleebester@gmail.com>
2024-04-08 21:24:31 +0000

    feat(index): Add LodgeOverview model & section

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class LodgeOverview(models.Model):
+        
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=400)
+    image = models.ImageField(upload_to='lodge_images/')
+
+    class Meta:
+        verbose_name = "Lodge overview"
+        verbose_name_plural = 'Lodge overview'
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import LodgeOverview
 
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'home/index.html')
+    lodges = LodgeOverview.objects.all()
+    return render(request, 'home/index.html', {'lodges': lodges})
