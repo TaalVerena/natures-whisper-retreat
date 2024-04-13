5edae80472873b5f174ca9e3dca7b09b47d6f39d
Sarah-Lee Bester <sarahleebester@gmail.com>
2024-04-13 15:39:09 +0000

    style(lodges): Style lodge detail layout

--- lodges/urls.py
+++ lodges/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.lodge_list, name='lodges'),
+    path('<int:pk>/', views.lodge_detail, name='lodge_detail'),
 ]
--- lodges/views.py
+++ lodges/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Lodge
 
 # Create your views here.
 def lodge_list(request):
     lodges = Lodge.objects.all()
     return render(request, 'lodges/lodge_list.html', {'lodges': lodges})
+
+# Detail view for a single lodge
+def lodge_detail(request, pk):
+    lodge = get_object_or_404(Lodge, pk=pk)
+    return render(request, 'lodges/lodge_detail.html', {'lodge': lodge})
