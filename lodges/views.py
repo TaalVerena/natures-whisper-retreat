b6a845732843af87739caa8541e0bda0fbb38825
Sarah-Lee Bester <sarahleebester@gmail.com>
2024-04-13 16:25:09 +0000

    style(lodges): Add pagination

--- lodges/views.py
+++ lodges/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Lodge
 
 # Create your views here.
 def lodge_list(request):
-    lodges = Lodge.objects.all()
-    return render(request, 'lodges/lodge_list.html', {'lodges': lodges})
+    lodges_list = Lodge.objects.all().order_by('name')
+    paginator = Paginator(lodges_list, 3) 
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number) 
+
+    return render(request, 'lodges/lodge_list.html', {'page_obj': page_obj})
 
 # Detail view for a single lodge
 def lodge_detail(request, pk):
