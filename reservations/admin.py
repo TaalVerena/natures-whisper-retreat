684bc9c0ba0f962b3e016cc1ef96e230807e3567
Sarah-Lee Bester <sarahleebester@gmail.com>
2024-04-15 21:30:10 +0000

    feat(res): Update res field in admin portal

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Reservation
 
-# Register your models here.
-admin.site.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('lodge', 'user', 'start_date', 'end_date', 'guests', 'duration_days')
+    list_filter = ('lodge', 'start_date', 'end_date', 'user')
+    search_fields = ('lodge__name', 'user__username', 'user__email')
+    date_hierarchy = 'start_date'
+
+    def duration_days(self, obj):
+        return (obj.end_date - obj.start_date).days
+    duration_days.short_description = 'Duration (days)'
+
+    # Optionally customize the form to use in the admin
+    # fields = ['user', 'lodge', 'start_date', 'end_date', 'guests']
+    # or use fieldsets for better organization
+
+admin.site.register(Reservation, ReservationAdmin)
