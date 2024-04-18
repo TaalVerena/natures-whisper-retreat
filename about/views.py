173d2fae221d13fde5430ddc2a836a936d6c777a
Sarah-Lee Bester <sarahleebester@gmail.com>
2024-04-18 19:42:37 +0000

    docs(about): Add comments

--- about/views.py
+++ about/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def about_view(request):
+    """
+    Renders the about page.
+
+    """
     return render(request, 'about/about.html')
