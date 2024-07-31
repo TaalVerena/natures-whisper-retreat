3c9dc916f77f4004043ef675b5257ee02a95ddb7
Sarah-Lee Bester <sarahleebester@gmail.com>
2024-07-31 20:09:16 +0000

    feat(dashboard): Add staff reservation view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import dashboard
-
+from .views import dashboard, reservation_list
 
 urlpatterns = [
     path("dashboard/", dashboard, name="dashboard"),
+    path("dashboard/reservations/", reservation_list, name="reservation_list"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,46 +1,57 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
 from datetime import datetime
 from reservations.models import Reservation
 from contact.forms import ContactForm
 
 
+def is_staff_user(user):
+    """
+    Check if the user is staff.
+    """
+    return user.is_staff
+
+
 @login_required
 def dashboard(request):
     """
-        Render user reservations and contact form.
+    Render user reservations and contact form.
 
-        Parameters:
-            request (HttpRequest): The HTTP request object.
-    a
-        Returns:
-            HttpResponse: Rendered HTML template for the user dashboard.
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: Rendered HTML template for the user dashboard.
     """
-    # Get the authenticated user
     user = request.user
-
-    # Get the current date
     current_date = datetime.now().date()
 
-    # Retrieve upcoming reservations for the user
     upcoming_reservations = Reservation.objects.filter(
         user=user, start_date__gte=current_date, status=Reservation.Status.CONFIRMED
     ).order_by("start_date")
 
-    # Retrieve past reservations for the user
     past_reservations = Reservation.objects.filter(
         user=user, start_date__lt=current_date, status=Reservation.Status.CONFIRMED
     ).order_by("-start_date")
 
-    # Instantiate a contact form for the user
     contact_form = ContactForm()
 
-    # Prepare the context data to be passed to the template
     context = {
         "upcoming_reservations": upcoming_reservations,
         "past_reservations": past_reservations,
         "contact_form": contact_form,
     }
 
-    # Render the dashboard template with the context data
     return render(request, "dashboard/dashboard.html", context)
+
+
+@login_required
+@user_passes_test(is_staff_user)
+def reservation_list(request):
+    """
+    Display a list of reservations for staff users.
+    """
+    reservations = Reservation.objects.all().order_by("-start_date")
+    return render(
+        request, "dashboard/reservation_list.html", {"reservations": reservations}
+    )
