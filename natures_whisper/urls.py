040a6b96b4ed20ae4c2cb818a8d896c8824595d6
Sarah-Lee Bester <sarahleebester@gmail.com>
2024-08-11 14:29:50 +0000

    maint(natures_whisper): Add comments & docstrings

--- natures_whisper/urls.py
+++ natures_whisper/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Main URL patterns for the natures_whisper project
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
--- natures_whisper/wsgi.py
+++ natures_whisper/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# Set the default Django settings module for the 'natures_whisper' project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'natures_whisper.settings')
 
+# Get the WSGI application for the project
 application = get_wsgi_application()
